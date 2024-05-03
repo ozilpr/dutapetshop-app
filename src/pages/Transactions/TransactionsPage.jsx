c5fa8d33659e276e50a3dc755f8c01a6bdb56f37
ozilpr <ozilp48@gmail.com>
2024-05-04 00:17:22 +0700

    add layout to transaction list

---
+++ src/pages/Transactions/TransactionsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import Trasanctions from '../../components/transactions/Trasanctions'
+const TransactionsPage = () => {
+  return (
+    <MainLayout>
+      <Trasanctions />
+    </MainLayout>
+  )
+}
+
+export default TransactionsPage
