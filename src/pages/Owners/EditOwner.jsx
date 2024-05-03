c6b900a55568fe478cc53eb5c3cf32de0146a76c
ozilpr <ozilp48@gmail.com>
2024-05-04 00:11:43 +0700

    add layout to form edit owner

---
+++ src/pages/Owners/EditOwner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import FormEditOwner from '../../components/Owners/FormEditOwner'
+
+const EditOwner = () => {
+  return (
+    <MainLayout>
+      <FormEditOwner />
+    </MainLayout>
+  )
+}
+
+export default EditOwner
