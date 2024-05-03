7c64d7636c612743e7733a8c73364c7bba5ef497
ozilpr <ozilp48@gmail.com>
2024-05-04 00:14:21 +0700

    add layout to pet data

---
+++ src/pages/Pets/PetById.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import GetPetById from '../../components/pets/GetPetById'
+
+const PetById = () => {
+  return (
+    <MainLayout>
+      <GetPetById />
+    </MainLayout>
+  )
+}
+
+export default PetById
