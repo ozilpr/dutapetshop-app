2dcfb72c036fff24e3ef65dde6d8d3d78b97b1ed
ozilpr <ozilp48@gmail.com>
2024-05-04 00:09:57 +0700

    add layout to form add admin

---
+++ src/pages/Admin/AddAdmin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout/MainLayout'
+import FormAddAdmin from '../../components/Admin/FormAddAdmin'
+
+const AddAdmin = () => {
+  return (
+    <Layout>
+      <FormAddAdmin />
+    </Layout>
+  )
+}
+
+export default AddAdmin
