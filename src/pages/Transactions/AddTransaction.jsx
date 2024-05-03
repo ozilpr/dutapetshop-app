ffce847cfd094d2327f812e4b170ef9acb9ba097
ozilpr <ozilp48@gmail.com>
2024-05-04 00:16:53 +0700

    add layout to form add transaction

---
+++ src/pages/Transactions/AddTransaction.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import FormAddTransaction from '../../components/transactions/FormAddTransaction'
+
+const AddTransaction = () => {
+  return (
+    <MainLayout>
+      <FormAddTransaction />
+    </MainLayout>
+  )
+}
+
+export default AddTransaction
