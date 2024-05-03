b9fd70321f5231285cbc8d53ba7765699178d7e3
ozilpr <ozilp48@gmail.com>
2024-05-04 00:15:05 +0700

    add layout to form add resource

---
+++ src/pages/Resources/AddResource.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import FormAddResource from '../../components/Resources/FormAddResource'
+
+const AddResource = () => {
+  return (
+    <MainLayout>
+      <FormAddResource />
+    </MainLayout>
+  )
+}
+
+export default AddResource
