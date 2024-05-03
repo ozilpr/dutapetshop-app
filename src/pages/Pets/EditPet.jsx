ca61cbadc9ff62458d40a4a0bff3f31251769e08
ozilpr <ozilp48@gmail.com>
2024-05-04 00:13:25 +0700

    add layout to form edit pet

---
+++ src/pages/Pets/EditPet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import FormEditPet from '../../components/pets/FormEditPet'
+
+const EditPet = () => {
+  return (
+    <MainLayout>
+      <FormEditPet />
+    </MainLayout>
+  )
+}
+
+export default EditPet
