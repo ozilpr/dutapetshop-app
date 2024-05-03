944dbddb11e89b6c4f136e26aeef04608fda03ff
ozilpr <ozilp48@gmail.com>
2024-05-04 00:17:07 +0700

    add layout to owner transaction list

---
+++ src/pages/Transactions/OwnerTransactionPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import OwnerTransaction from '../../components/transactions/OwnerTransaction'
+
+const OwnerTransactionPage = () => {
+  return (
+    <MainLayout>
+      <OwnerTransaction />
+    </MainLayout>
+  )
+}
+
+export default OwnerTransactionPage
