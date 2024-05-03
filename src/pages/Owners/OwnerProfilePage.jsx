bfe2c6035cb8ddeef6aab2919cf49f0cc4352012
ozilpr <ozilp48@gmail.com>
2024-05-04 00:12:04 +0700

    add layout to owner profile

---
+++ src/pages/Owners/OwnerProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import OwnerProfile from '../../components/Owners/OwnerProfile'
+
+const OwnerProfilePage = () => {
+  return (
+    <MainLayout>
+      <OwnerProfile />
+    </MainLayout>
+  )
+}
+
+export default OwnerProfilePage
