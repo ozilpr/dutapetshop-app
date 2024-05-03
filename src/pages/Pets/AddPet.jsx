cd86235f9bd847f3a39b09d728c6708c95756a6a
ozilpr <ozilp48@gmail.com>
2024-05-04 00:12:40 +0700

    add layout to form add pet

---
+++ src/pages/Pets/AddPet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import FormAddPet from '../../components/pets/FormAddPet'
+
+const AddPet = () => {
+  return (
+    <MainLayout>
+      <FormAddPet />
+    </MainLayout>
+  )
+}
+
+export default AddPet
