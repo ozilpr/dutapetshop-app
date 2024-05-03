18bcf7e8234cd3dde78011c69a0100a869c72627
ozilpr <ozilp48@gmail.com>
2024-05-04 00:12:57 +0700

    add layout to form add pet owner

---
+++ src/pages/Pets/AddPetOwner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import FormAddPetOwner from '../../components/pets/FormAddPetOwner'
+
+const AddPetOwner = () => {
+  return (
+    <MainLayout>
+      <FormAddPetOwner />
+    </MainLayout>
+  )
+}
+
+export default AddPetOwner
