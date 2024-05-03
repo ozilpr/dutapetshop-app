9bd6a549497d0aad0a581fc14e8ca2bb84fab47a
ozilpr <ozilp48@gmail.com>
2024-05-04 00:10:22 +0700

    add layout to admin list

---
+++ src/pages/Admin/AdminPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout/MainLayout'
+import Admin from '../../components/Admin/Admin'
+
+const AdminPage = () => {
+  return (
+    <Layout>
+      <Admin />
+    </Layout>
+  )
+}
+
+export default AdminPage
