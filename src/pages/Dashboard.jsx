ec65eacbb5a3eccc423fd7f83e0e8ccee7889ba3
ozilpr <ozilp48@gmail.com>
2024-05-04 00:09:02 +0700

    just a greetings

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainLayout from '../components/Layout/MainLayout'
+
+const Dashboard = () => {
+  return (
+    <MainLayout>
+      <div>
+        <h1 className="title">Dashboard</h1>
+        <h2 className="subtitle">Selamat Datang</h2>
+      </div>
+    </MainLayout>
+  )
+}
+
+export default Dashboard
