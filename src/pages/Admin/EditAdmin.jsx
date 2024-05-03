319e9b1444ab9bea7a83ae8f1a03601e347eafb8
ozilpr <ozilp48@gmail.com>
2024-05-04 00:10:40 +0700

    add layout to form edit admin

---
+++ src/pages/Admin/EditAdmin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout/MainLayout'
+import FormEditAdmin from '../../components/Admin/FormEditAdmin'
+
+const EditAdmin = () => {
+  return (
+    <Layout>
+      <FormEditAdmin />
+    </Layout>
+  )
+}
+
+export default EditAdmin
