fe2e02ff54efed205bc9f8ea911bcaeb07857065
ozilpr <ozilp48@gmail.com>
2024-05-04 00:16:36 +0700

    add layout to resource list

---
+++ src/pages/Resources/ResourcesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import Resources from '../../components/Resources/Resources'
+
+const ResourcesPage = () => {
+  return (
+    <MainLayout>
+      <Resources />
+    </MainLayout>
+  )
+}
+
+export default ResourcesPage
