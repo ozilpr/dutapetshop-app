33c805a3648ca71ce60180b5467d3f5341a3b86d
ozilpr <ozilp48@gmail.com>
2024-05-04 00:14:37 +0700

    add layout to pet list

---
+++ src/pages/Pets/PetsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import Pets from '../../components/pets/Pets'
+
+const PetsPage = () => {
+  return (
+    <MainLayout>
+      <Pets />
+    </MainLayout>
+  )
+}
+
+export default PetsPage
