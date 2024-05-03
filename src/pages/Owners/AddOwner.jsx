0af8b64b9f73ad3991d41d5ef6b5816babee4664
ozilpr <ozilp48@gmail.com>
2024-05-04 00:11:00 +0700

    add layout to form add owner

---
+++ src/pages/Owners/AddOwner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../../components/Layout/MainLayout'
+import FormAddOwner from '../../components/Owners/FormAddOwner'
+
+const AddOwner = () => {
+  return (
+    <MainLayout>
+      <FormAddOwner />
+    </MainLayout>
+  )
+}
+
+export default AddOwner
